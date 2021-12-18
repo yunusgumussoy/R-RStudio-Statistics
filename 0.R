cd6ff4d183c8af2086e18915560c7348ec387119
Yunus GÜMÜŞSOY <34836874+yunusgumussoy@users.noreply.github.com>
2021-12-18 11:35:15 +0300

    Add files via upload

---
+++ 0.R
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: MIT
+# Yunus GUMUSSOY - The First Step of Being a Data Scientist
+
+
+print ("Hello World!")
+
+#yorum satýrý
+print ("Hello World! My Name is Yunus ")
+
+# ? kullanarak yardým alabiliriz
+?plot #ctrl enter kodu çalýþtýrýr
+
+#sadece enter satýr atlar
+help(print)
+help(mean)
+
+#package yüklemek için
+library (vcd)
+
+require(vcd)
+
+# package iptal etmek için detach (vcd)
